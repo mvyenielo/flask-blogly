dda2103721245dd909f0b6f46f601a57bfc26d4c
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-08-30 15:41:46 -0700

    added in html templates for all pages, created first few routes

--- app.py
+++ app.py
@@ -13,3 +13,30 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+q = User.query
+
+@app.get('/')
+def render_user_list():
+    """redirects to list of users"""
+
+    return redirect('/users')
+
+@app.get('/users')
+def render_user_list():
+    """Renders list of users/add user button"""
+
+    users = q.all()
+
+    return render_template('index.html',
+                           users = users)
+
+@app.get('/users/new')
+def render_user_list():
+    """"""
+
+    users = q.all()
+
+    render_template('user_new.html',
+                    users = users)
+
